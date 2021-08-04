8b00ee2851a9c476122f4b6aef29e5696207f454
Beckie Doyle <beckiedoyle@hotmail.co.uk>
2021-08-04 16:51:31 +0100

    On implementing hit points - completed first unit test

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -1,7 +1,16 @@
 feature 'Attack' do
-    scenario 'attacking player 2' do
-      sign_in_and_play
-      click_button "Attack"
-      expect(page).to have_content 'Phineas attacked Ferb!'
-    end
+  scenario 'attacking player 2' do
+    sign_in_and_play
+    click_button "Attack"
+    expect(page).to have_content 'Phineas attacked Ferb!'
   end
+end
+
+feature 'Attacking' do
+  scenario 'reduces score by 10 when player is attacked' do
+    sign_in_and_play
+    click_button "Attack"
+    expect(page).not_to have_content "Ferb: 60HP"
+    expect(page).to have_content "Ferb: 50HP"
+  end
+end
--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -8,4 +8,11 @@ require 'player'
         expect(phineas.name).to eq "Phineas"
       end
     end
+
+    describe "#hit_points" do
+      it "returns the hit points" do
+        expect(phineas.hit_points).to eq described_class::DEFAULT_HIT_POINTS
+      end
+    end
+
   end
